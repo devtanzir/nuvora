aabac31b74fd8cd960ee597f97774bca0658c814
tanzir ibne ali <tanziribneali@gmail.com>
2026-04-26 00:28:57 +0600

    database migration complete with mysql docker

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,18 +1,8 @@
-import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
-import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-
-
-  constructor() {
-    const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL,
-    });
-    super({adapter});
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
